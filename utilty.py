559889f401732318dbc8d854d6722ad8b9a0de06
spin733 <spin73333@gmail.com>
2024-08-05 17:53:12 +0900

    함수만들었어요!

---
+++ utilty.py
@@ -0,0 +1,28 @@
+# 한 줄 짜리 주석
+"""
+코드작성일자
+코드작성자
+코드아름
+코드목적
+
+"""
+
+
+def factorial(x):
+    if x <= 1:
+       return 1
+    return x * factorial(x-1)
+
+
+def gugudan(x):
+    for i in range(9):
+              print((i + 1) * x)
+
+
+
+
+
+
+
+
+
